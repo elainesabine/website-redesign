117f1f7949d92b0942e74b508285d18c630b36e0
Elaine Pranadjaya <elainesabine@gmail.com>
2019-07-24 22:49:44 +0700

    yay

---
+++ elainesabine.js
@@ -0,0 +1,125 @@
+// clicking on the navbar buttons
+$("#navbarContainer div").on("click", function (event) {
+    $("#navbarContainer div #aboutmeID").addClass("aboutMeSpan");
+    $("#navbarContainer div #experienceID").addClass("experienceSpan");
+    $("#navbarContainer div #projectsID").addClass("projectsSpan");
+    $("#navbarContainer div #clubsID").addClass("clubsSpan");
+
+    $("#navbarContainer div span").removeClass("selected");
+    $(this).children("span").addClass("selected");
+    
+    if ($(this).hasClass("aboutMe")){
+        $("#navbarContainer div #aboutMeID").removeClass("aboutMeSpan");
+    } else if ($(this).hasClass("experience")){
+        $("#navbarContainer div #experienceID").removeClass("experienceSpan");
+    } else if ($(this).hasClass("projects")){
+        $("#navbarContainer div #projectsID").removeClass("projectsSpan");
+    } else if ($(this).hasClass("clubs")){
+        $("#navbarContainer div #clubsID").removeClass("clubsSpan");
+    } 
+});
+
+
+// hovering over navbar buttons
+$("#navbarContainer div").on("mouseenter", function () {
+    $(this).children("span").addClass("hover")
+});
+
+$("#navbarContainer div").on("mouseleave", function () {
+    $(this).children("span").removeClass("hover")
+});
+
+
+// clicking on about me
+$(".aboutMe").on("click", function () {
+    $(".pages").css("display", "none")
+    $("#aboutMeTop").css("display", "block")
+    $("#aboutMeBottom").css("display", "block")
+});
+
+
+// clicking on experience
+$(".experience").on("click", function () {
+    $(".pages").css("display", "none")
+    $("#experienceTop").css("display", "block")
+    $("#experienceBottom").css("display", "block")
+});
+
+// clicking on projects
+$(".projects").on("click", function () {
+    $(".pages").css("display", "none")
+    $("#projectsTop").css("display", "block")
+    $("#projectsBottom").css("display", "block")
+});
+
+// clicking on clubs
+$(".clubs").on("click", function () {
+    $(".pages").css("display", "none")
+    $("#clubsTop").css("display", "block")
+    $("#clubsBottom").css("display", "block")
+});
+
+
+var clickCountProjects = 0;
+var totalProjectsNum = 3;
+//clicking on left arrow in projects page
+$("#leftarrowproject").on("click", function () {
+    clickCountProjects --;
+
+    //if the number is negative
+    if (clickCountProjects % totalProjectsNum < 0) {
+        clickCountProjects = Math.abs(clickCountProjects - 1 )  
+    }
+    
+    if (clickCountProjects % totalProjectsNum == 0) {
+        $('.project').fadeOut(500) 
+        $('#project1').delay(500).fadeIn(500)
+    } else if (clickCountProjects % totalProjectsNum == 1) {
+        $('.project').fadeOut(500)
+        $('#project2').delay(500).fadeIn(500)
+    } else if (clickCountProjects % totalProjectsNum == 2) {
+        $('.project').fadeOut(500)
+        $('#project3').delay(500).fadeIn(500)
+    };
+
+    $("#projectnum").text(clickCountProjects % totalProjectsNum + 1); // update the pager
+});
+
+//clicking on right arrow in projects page
+$("#rightarrowproject").on("click", function () {
+    clickCountProjects ++;
+    if (clickCountProjects % totalProjectsNum == 0) {
+        $('.project').fadeOut(500)
+        $('#project1').delay(500).fadeIn(500)
+    } else if (clickCountProjects % totalProjectsNum == 1) {
+        $('.project').fadeOut(500)
+        $('#project2').delay(500).fadeIn(500)
+    } else if (clickCountProjects % totalProjectsNum == 2) {
+        $('.project').fadeOut(500)
+        $('#project3').delay(500).fadeIn(500)
+    }
+    $("#projectnum").text(clickCountProjects % totalProjectsNum + 1);
+});
+
+
+// projects arrow click to change slideshow
+$(document).ready(function() {
+    var slideshow;
+    slideshow = $('#slideshow').wipe();
+    slideshow.pause();
+
+    $('#leftarrowproject').click(function(event) {
+        event.preventDefault();
+        slideshow.prev();
+        slideshow.pause();
+        // alert("left");
+    });
+
+    $('#rightarrowproject').click(function(event) {
+        event.preventDefault();
+        slideshow.next();
+        slideshow.pause();
+        // alert("right ")
+    });
+});
+
